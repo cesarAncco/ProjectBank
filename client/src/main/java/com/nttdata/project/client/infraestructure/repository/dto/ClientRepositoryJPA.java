3a2132cce26a7d927885231c1570a07dea139d6b
cesarAncco <canccor@gmail.com>
2024-05-19 13:07:19 -0500

    Commit Rename: Changing the name of the class where we extend JPA.

--- client/src/main/java/com/nttdata/project/client/infraestructure/repository/dto/ClientRepositoryJPA.java
+++ client/src/main/java/com/nttdata/project/client/infraestructure/repository/dto/ClientRepositoryJPA.java
@@ -1,8 +1,8 @@
 package com.nttdata.project.client.infraestructure.repository.dto;
 
 import com.nttdata.project.client.domain.dto.entity.ClientEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientRepository extends CrudRepository<ClientEntity, Long> {
+public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
     public ClientEntity findByDocument(String document);
 }
